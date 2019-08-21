920aeec2f9d3a1289cdd8669a326778bf6c1782a
lgandecki <lgandecki@thebrain.pro>
2019-08-21 12:45:56 +0400

    test(before and after): more human-readable (hopefully!) feature steps

--- cypress/integration/BeforeAndAfterSteps.feature
+++ cypress/integration/BeforeAndAfterSteps.feature
@@ -1,35 +1,32 @@
 Feature: Using Before and After
 
-# After is implicitly tested by resetting the counter.
+Scenario: With Untagged Before
+    Then Untagged Before was called once
+    And Tagged Before was not called
 
-Scenario: With Before only
-    Given I do something
-    And Something else
-    Then Before was called once
+# After is tested in following scenario by verifying that the After ran at the end of the previous one
+Scenario: With Untagged After part 1
+  Given I executed empty step
+
+Scenario: With Untagged After part 2
+  Then Flag should be set by untagged After
+
+# After is tested in following scenario by verifying that the After ran at the end of the previous one.
+@withTaggedAfter
+Scenario: With tagged After part 1
+  Given I executed empty step
+
+Scenario: With tagged After part 2
+  Then Flag should be set by tagged After
 
 @withTaggedBefore
 Scenario: With tagged Before only
-    Given I do something
-    And Something else
-    Then Before with tag was called once
-
-Scenario: With After only
-    Given I do something
-    And Something else
-
-@withTaggedAfter
-Scenario: With tagged After only
-    Given I do something
-    And Something else
-
-Scenario: With Before and After
-    Given I do something
-    And Something else
-    Then Before was called once
+  Then Tagged Before was called once
+  And Untagged Before was called once
 
 @withTaggedBefore
-@withTaggedAfter
-Scenario: With tagged After only
-    Given I do something
-    And Something else
-    Then Before with tag was called once
+@withAnotherTaggedBefore
+Scenario: With multiple tags
+    Given I executed empty step
+    Then Tagged Before was called twice
+    And Untagged Before was called once
--- cypress/support/step_definitions/before_and_after_steps.js
+++ cypress/support/step_definitions/before_and_after_steps.js
@@ -4,18 +4,24 @@ const {
   Before,
   After,
   Given,
-  Then,
-  When
+  Then
 } = require("cypress-cucumber-preprocessor/steps");
 
 let beforeCounter = 0;
 let beforeWithTagCounter = 0;
+let flagSetByUntaggedAfter = false;
+let flagSetByTaggedAfter = false;
 
 Before(() => {
   beforeCounter += 1;
+  beforeWithTagCounter = 0;
 });
 
 Before({ tags: "@withTaggedBefore" }, () => {
+  beforeWithTagCounter += 1;
+});
+
+Before({ tags: "@withAnotherTaggedBefore" }, () => {
   beforeWithTagCounter += 1;
 });
 
@@ -29,24 +35,39 @@ After({ tags: "@willNeverRun" }, () => {
 
 After(() => {
   beforeCounter = 0;
+  flagSetByUntaggedAfter = true;
 });
 
 After({ tags: "@withTaggedAfter" }, () => {
-  beforeWithTagCounter = 0;
+  flagSetByTaggedAfter = true;
 });
 
-Given("I do something", () => {});
+Given("I executed empty step", () => {});
 
-When("Something else", () => {});
-
-Then("Before was called once", () => {
+Then("Untagged Before was called once", () => {
   expect(beforeCounter).to.equal(1);
 });
 
-Then("Before with tag was called once", () => {
+Then("Untagged Before was not called", () => {
+  expect(beforeCounter).to.equal(0);
+});
+
+Then("Tagged Before was called once", () => {
   expect(beforeWithTagCounter).to.equal(1);
 });
 
-Then("Before was not called", () => {
-  expect(beforeCounter).to.equal(1);
+Then("Tagged Before was called twice", () => {
+  expect(beforeWithTagCounter).to.equal(2);
+});
+
+Then("Tagged Before was not called", () => {
+  expect(beforeWithTagCounter).to.equal(0);
+});
+
+Then("Flag should be set by untagged After", () => {
+  expect(flagSetByUntaggedAfter).to.equal(true);
+});
+
+Then("Flag should be set by tagged After", () => {
+  expect(flagSetByTaggedAfter).to.equal(true);
 });
