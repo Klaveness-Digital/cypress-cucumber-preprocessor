Klaveness-Digital/cypress-cucumber-preprocessor

commit 3292cf52f6747370b8ecd20ee274647e4a598d36

Change configuration in accordance with previous comment [1]
[1] https://github.com/TheBrainFamily/cypress-cucumber-preprocessor/issues/482#issuecomment-740474987

features/support/hooks.js
@@ -36,23 +36,23 @@ Before(async function ({ gherkinDocument, pickle }) {
       const { compile } = require("${projectPath}/lib/template");
       const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
 
-      const FeatureOnLoadPlugin = {
-        name: "feature",
-        setup(build) {
-          const fs = require("fs");
-
-          build.onLoad({ filter: /\.feature$/ }, async args => {
-            const content = await fs.promises.readFile(args.path, "utf8");
-
-            return {
-              contents: await compile(content, args.path),
-              loader: "js"
-            };
-          });
-        }
-      };
-
       module.exports = (on, config) => {
+        const FeatureOnLoadPlugin = {
+          name: "feature",
+          setup(build) {
+            const fs = require("fs");
+
+            build.onLoad({ filter: /\.feature$/ }, async args => {
+              const content = await fs.promises.readFile(args.path, "utf8");
+
+              return {
+                contents: await compile(config, content, args.path),
+                loader: "js"
+              };
+            });
+          }
+        };
+
         on(
           "file:preprocessor",
           createBundler({

lib/index.ts
@@ -10,6 +10,8 @@ import debug from "./debug";
 
 import { compile } from "./template";
 
+import { ICypressConfiguration } from "./cypress-configuration";
+
 declare global {
   interface Window {
     testState: {
@@ -20,7 +22,10 @@ declare global {
   }
 }
 
-export function transform(filepath: string) {
+export function transform(
+  configuration: ICypressConfiguration,
+  filepath: string
+) {
   if (!filepath.match(".feature$")) {
     return new PassThrough();
   }
@@ -36,7 +41,10 @@ export function transform(filepath: string) {
     },
     async flush(done: TransformCallback) {
       try {
-        done(null, await compile(buffer.toString("utf8"), filepath));
+        done(
+          null,
+          await compile(configuration, buffer.toString("utf8"), filepath)
+        );
 
         debug(`compiled ${filepath}`);
       } catch (e) {
@@ -83,6 +91,8 @@ export function preprocessor(options = browserify.defaultOptions) {
     return browserify(options)(file);
   };
 }
+
+export { ICypressConfiguration };
 
 export { compile };
 

features/step_definitions.feature
@@ -1,0 +1,48 @@
+Feature: step definitions
+
+  Rule: it should by default look for step definitions in a couple of locations
+
+    Example: step definitions with same filename
+      Given a file named "cypress/integration/a.feature" with:
+        """
+        Feature: a feature name
+          Scenario: a scenario name
+            Given a step
+        """
+      And a file named "cypress/integration/a.js" with:
+        """
+        const { Given } = require("@badeball/cypress-cucumber-preprocessor/methods");
+        Given("a step", function() {});
+        """
+      When I run cypress
+      Then it passes
+
+    Example: step definitions in a directory with same name
+      Given a file named "cypress/integration/a.feature" with:
+        """
+        Feature: a feature name
+          Scenario: a scenario name
+            Given a step
+        """
+      And a file named "cypress/integration/a/steps.js" with:
+        """
+        const { Given } = require("@badeball/cypress-cucumber-preprocessor/methods");
+        Given("a step", function() {});
+        """
+      When I run cypress
+      Then it passes
+
+    Example: step definitions in a common directory
+      Given a file named "cypress/integration/a.feature" with:
+        """
+        Feature: a feature name
+          Scenario: a scenario name
+            Given a step
+        """
+      And a file named "cypress/support/step_definitions/steps.js" with:
+        """
+        const { Given } = require("@badeball/cypress-cucumber-preprocessor/methods");
+        Given("a step", function() {});
+        """
+      When I run cypress
+      Then it passes

lib/cypress-configuration.ts
@@ -23,7 +23,8 @@ function isStringEntry(entry: [any, any]): entry is [string, string] {
  *
  * Definitions can found in https://github.com/cypress-io/cypress/blob/develop/cli/schema/cypress.schema.json.
  */
-interface ICypressConfiguration {
+export interface ICypressConfiguration {
+  projectRoot: string;
   integrationFolder: string;
   fixturesFolder: string | false;
   supportFile: string | false;
@@ -36,6 +37,13 @@ function validateConfigurationEntry(
   value: unknown
 ): Partial<ICypressConfiguration> {
   switch (key) {
+    case "projectRoot":
+      if (!isString(value)) {
+        throw new Error(
+          `Expected a string (projectRoot), but got ${util.inspect(value)}`
+        );
+      }
+      return { [key]: value };
     case "integrationFolder":
       if (!isString(value)) {
         throw new Error(
@@ -254,6 +262,7 @@ export function resolveConfiguration(options: {
 
   const configuration = Object.assign(
     {
+      projectRoot: resolveProjectPath(options),
       integrationFolder: "cypress/integration",
       fixturesFolder: "cypress/fixtures",
       supportFile: "cypress/support/index.js",

lib/preprocessor-configuration.ts
@@ -4,44 +4,17 @@ import util from "util";
 
 import debug from "./debug";
 
-import { isBoolean, isString } from "./type-guards";
+import { isStringOrStringArray } from "./type-guards";
 
 function validateConfigurationEntry(
   key: string,
   value: unknown
 ): Partial<IPreprocessorConfiguration> {
   switch (key) {
-    case "globalStepDefinitions":
-      if (!isBoolean(value)) {
+    case "stepDefinitions":
+      if (!isStringOrStringArray(value)) {
         throw new Error(
-          `Expected a string (globalStepDefinitions), but got ${util.inspect(
-            value
-          )}`
-        );
-      }
-      return { [key]: value };
-    case "stepDefinitionsFolder":
-      if (!isString(value)) {
-        throw new Error(
-          `Expected a string (stepDefinitionsFolder), but got ${util.inspect(
-            value
-          )}`
-        );
-      }
-      return { [key]: value };
-    case "stepDefinitionsCommonFolder":
-      if (!isString(value)) {
-        throw new Error(
-          `Expected a string (stepDefinitionsCommonFolder), but got ${util.inspect(
-            value
-          )}`
-        );
-      }
-      return { [key]: value };
-    case "integrationFolder":
-      if (!isString(value)) {
-        throw new Error(
-          `Expected a string (integrationFolder), but got ${util.inspect(
+          `Expected a string or array of strings (stepDefinitions), but got ${util.inspect(
             value
           )}`
         );
@@ -53,39 +26,20 @@ function validateConfigurationEntry(
 }
 
 export interface IPreprocessorConfiguration {
-  readonly globalStepDefinitions: boolean;
-  readonly stepDefinitionsFolder: string;
-  readonly stepDefinitionsCommonFolder: string;
-  readonly integrationFolder: string;
+  readonly stepDefinitions: string | string[];
 }
 
 export class PreprocessorConfiguration implements IPreprocessorConfiguration {
   constructor(private explicitValues: Partial<IPreprocessorConfiguration>) {}
 
-  get globalStepDefinitions() {
-    if (this.explicitValues.globalStepDefinitions === false) {
-      return false;
-    }
-
-    return this.explicitValues.globalStepDefinitions || true;
-  }
-
-  get stepDefinitionsFolder() {
-    if (this.explicitValues.stepDefinitionsFolder) {
-      return this.explicitValues.stepDefinitionsFolder;
-    } else if (this.globalStepDefinitions) {
-      return "cypress/support/step_definitions";
-    } else {
-      return this.integrationFolder;
-    }
-  }
-
-  get stepDefinitionsCommonFolder() {
-    return this.explicitValues.stepDefinitionsCommonFolder || "common";
-  }
-
-  get integrationFolder() {
-    return this.explicitValues.integrationFolder || "cypress/integration";
+  get stepDefinitions() {
+    return (
+      this.explicitValues.stepDefinitions || [
+        "cypress/integration/[filepath]/**/*",
+        "cypress/integration/[filepath].*",
+        "cypress/support/step_definitions/**/*",
+      ]
+    );
   }
 }
 

lib/step-definitions.test.ts
@@ -2,26 +2,35 @@ import util from "util";
 
 import assert from "assert";
 
+import { ICypressConfiguration } from "./cypress-configuration";
+
 import {
-  PreprocessorConfiguration,
   IPreprocessorConfiguration,
+  PreprocessorConfiguration,
 } from "./preprocessor-configuration";
 
-import { getStepDefinitionDirectories } from "./step-definitions";
+import { getStepDefinitionPatterns } from "./step-definitions";
 
 function example(
   filepath: string,
-  cwd: string,
-  explicitValues: Partial<IPreprocessorConfiguration>,
+  cypressConfiguration: Pick<
+    ICypressConfiguration,
+    "projectRoot" | "integrationFolder"
+  >,
+  preprocessorConfiguration: Partial<IPreprocessorConfiguration>,
   expected: string[]
 ) {
   it(`should return [${expected.join(
     ", "
-  )}] for ${filepath} with ${util.inspect(explicitValues)} in ${cwd}`, () => {
-    const actual = getStepDefinitionDirectories(
-      filepath,
-      new PreprocessorConfiguration(explicitValues),
-      cwd
+  )}] for ${filepath} with ${util.inspect(preprocessorConfiguration)} in ${
+    cypressConfiguration.projectRoot
+  }`, () => {
+    const actual = getStepDefinitionPatterns(
+      {
+        cypress: cypressConfiguration,
+        preprocessor: new PreprocessorConfiguration(preprocessorConfiguration),
+      },
+      filepath
     );
 
     const throwUnequal = () => {
@@ -42,76 +51,51 @@ function example(
   });
 }
 
-describe("getStepDefinitionDirectories()", () => {
-  example("/foo/bar/cypress/integration/baz.feature", "/foo/bar", {}, [
-    "/foo/bar/cypress/support/step_definitions",
-  ]);
-
+describe("getStepDefinitionPatterns()", () => {
   example(
     "/foo/bar/cypress/integration/baz.feature",
-    "/foo/bar",
-    { stepDefinitionsFolder: "cypress/integration/step_definitions" },
-    ["/foo/bar/cypress/integration/step_definitions"]
-  );
-
-  example(
-    "/foo/bar/cypress/integration/baz.feature",
-    "/foo/bar",
     {
-      globalStepDefinitions: false,
+      projectRoot: "/foo/bar",
+      integrationFolder: "cypress/integration",
     },
-    ["/foo/bar/cypress/integration/baz", "/foo/bar/cypress/integration/common"]
-  );
-
-  example(
-    "/foo/bar/cypress/integration/baz.feature",
-    "/foo/bar",
-    {
-      globalStepDefinitions: false,
-      stepDefinitionsFolder: "cypress/support",
-    },
-    ["/foo/bar/cypress/support/baz", "/foo/bar/cypress/support/common"]
-  );
-
-  example(
-    "/foo/bar/cypress/integration/baz.feature",
-    "/foo/bar",
-    {
-      globalStepDefinitions: false,
-      stepDefinitionsCommonFolder: "universal",
-    },
+    {},
     [
-      "/foo/bar/cypress/integration/baz",
-      "/foo/bar/cypress/integration/universal",
+      "/foo/bar/cypress/integration/baz/**/*",
+      "/foo/bar/cypress/integration/baz.*",
+      "/foo/bar/cypress/support/step_definitions/**/*",
     ]
-  );
-
-  example(
-    "/foo/bar/cypress/features/baz.feature",
-    "/foo/bar",
-    {
-      globalStepDefinitions: false,
-      integrationFolder: "cypress/features",
-    },
-    ["/foo/bar/cypress/features/baz", "/foo/bar/cypress/features/common"]
   );
 
   it("should error when provided a path not within integrationFolder", () => {
     assert.throws(() => {
-      getStepDefinitionDirectories(
-        "/foo/bar/cypress/features/baz.feature",
-        new PreprocessorConfiguration({}),
-        "/foo/bar"
+      getStepDefinitionPatterns(
+        {
+          cypress: {
+            projectRoot: "/foo/bar",
+            integrationFolder: "cypress/integration",
+          },
+          preprocessor: {
+            stepDefinitions: [],
+          },
+        },
+        "/foo/bar/cypress/features/baz.feature"
       );
     }, "/foo/bar/cypress/features/baz.feature is not within cypress/integration");
   });
 
   it("should error when provided a path not within cwd", () => {
     assert.throws(() => {
-      getStepDefinitionDirectories(
-        "/foo/bar/cypress/integration/baz.feature",
-        new PreprocessorConfiguration({}),
-        "/baz"
+      getStepDefinitionPatterns(
+        {
+          cypress: {
+            projectRoot: "/baz",
+            integrationFolder: "cypress/integration",
+          },
+          preprocessor: {
+            stepDefinitions: [],
+          },
+        },
+        "/foo/bar/cypress/integration/baz.feature"
       );
     }, "/foo/bar/cypress/features/baz.feature is not within /baz");
   });

lib/step-definitions.ts
@@ -8,30 +8,29 @@ import isPathInside from "is-path-inside";
 
 import debug from "./debug";
 
-import {
-  IPreprocessorConfiguration,
-  resolve,
-} from "./preprocessor-configuration";
+import { ICypressConfiguration } from "./cypress-configuration";
+
+import { IPreprocessorConfiguration } from "./preprocessor-configuration";
 
 export async function getStepDefinitionPaths(
+  configuration: {
+    cypress: ICypressConfiguration;
+    preprocessor: IPreprocessorConfiguration;
+  },
   filepath: string
 ): Promise<string[]> {
-  const configuration = await resolve();
-
-  const patterns = getStepDefinitionDirectories(filepath, configuration).map(
-    (directory) => `${directory}/**/*.@(js|ts)`
-  );
-
-  debug(`looking for step definitions using ${patterns.join(", ")}`);
-
   const files = (
-    await Promise.all(patterns.map((pattern) => util.promisify(glob)(pattern)))
+    await Promise.all(
+      getStepDefinitionPatterns(configuration, filepath).map((pattern) =>
+        util.promisify(glob)(pattern, { nodir: true })
+      )
+    )
   ).reduce((acum, el) => acum.concat(el), []);
 
   if (files.length === 0) {
     debug("found no step definitions");
   } else {
-    debug(`found step definitions in ${files.join(", ")}`);
+    debug(`found step definitions ${util.inspect(files)}`);
   }
 
   return files;
@@ -41,40 +40,51 @@ function trimFeatureExtension(filepath: string) {
   return filepath.replace(/\.feature$/, "");
 }
 
-export function getStepDefinitionDirectories(
-  filepath: string,
-  configuration: IPreprocessorConfiguration,
-  cwd: string = process.cwd()
+export function getStepDefinitionPatterns(
+  configuration: {
+    cypress: Pick<ICypressConfiguration, "projectRoot" | "integrationFolder">;
+    preprocessor: IPreprocessorConfiguration;
+  },
+  filepath: string
 ): string[] {
-  const fullIntegrationFolder = path.join(cwd, configuration.integrationFolder);
+  const fullIntegrationFolder = path.isAbsolute(
+    configuration.cypress.integrationFolder
+  )
+    ? configuration.cypress.integrationFolder
+    : path.join(
+        configuration.cypress.projectRoot,
+        configuration.cypress.integrationFolder
+      );
 
   if (!isPathInside(filepath, fullIntegrationFolder)) {
     throw new Error(`${filepath} is not inside ${fullIntegrationFolder}`);
   }
 
-  if (!isPathInside(filepath, cwd)) {
-    throw new Error(`${filepath} is not inside ${cwd}`);
-  }
-
-  if (configuration.globalStepDefinitions) {
-    return [path.join(cwd, configuration.stepDefinitionsFolder)];
-  } else {
-    const relativeFilepath = path.relative(
-      path.join(cwd, configuration.integrationFolder),
-      filepath
+  if (!isPathInside(filepath, configuration.cypress.projectRoot)) {
+    throw new Error(
+      `${filepath} is not inside ${configuration.cypress.projectRoot}`
     );
-
-    return [
-      path.join(
-        cwd,
-        configuration.stepDefinitionsFolder,
-        trimFeatureExtension(relativeFilepath)
-      ),
-      path.join(
-        cwd,
-        configuration.stepDefinitionsFolder,
-        configuration.stepDefinitionsCommonFolder
-      ),
-    ];
   }
+
+  debug(
+    `looking for step definitions using ${util.inspect(
+      configuration.preprocessor.stepDefinitions
+    )}`
+  );
+
+  const filepathReplacement = trimFeatureExtension(
+    path.relative(fullIntegrationFolder, filepath)
+  );
+
+  debug(`replacing [filepath] with ${util.inspect(filepathReplacement)}`);
+
+  return (typeof configuration.preprocessor.stepDefinitions === "string"
+    ? [configuration.preprocessor.stepDefinitions]
+    : configuration.preprocessor.stepDefinitions
+  ).map((pattern) =>
+    path.join(
+      configuration.cypress.projectRoot,
+      pattern.replace("[filepath]", filepathReplacement)
+    )
+  );
 }

lib/template.ts
@@ -4,6 +4,10 @@ import { IdGenerator } from "@cucumber/messages";
 
 import { assertAndReturn } from "./assertions";
 
+import { ICypressConfiguration } from "./cypress-configuration";
+
+import { resolve } from "./preprocessor-configuration";
+
 import { getStepDefinitionPaths } from "./step-definitions";
 
 function notNull<T>(value: T | null | undefined): value is T {
@@ -12,6 +16,7 @@ function notNull<T>(value: T | null | undefined): value is T {
 
 export async function compile(
   this: any,
+  configuration: ICypressConfiguration,
   data: string,
   uri: string = this.resourcePath
 ) {
@@ -40,7 +45,13 @@ export async function compile(
 
   const pickles = envelopes.map((envelope) => envelope.pickle).filter(notNull);
 
-  const stepDefinitions = await getStepDefinitionPaths(uri);
+  const stepDefinitions = await getStepDefinitionPaths(
+    {
+      cypress: configuration,
+      preprocessor: await resolve(),
+    },
+    uri
+  );
 
   return `
     const createTests = require("@badeball/cypress-cucumber-preprocessor/lib/create-tests");

commit 0f35ab95529f76f7eff18801ce6eb92297651e78

Fix and test support for webpack + browserify
Support for browserify was broken since 3292cf5 [1] and it turns out
that webpack has never really been supported. Async loaders for webpack
shouldn't return promises, but rather invoke a callback [2].

[1] https://github.com/badeball/cypress-cucumber-preprocessor/commit/3292cf5
[2] https://v4.webpack.js.org/api/loaders/#asynchronous-loaders

browserify.ts
@@ -1,0 +1,1 @@
+export * from "./lib";

features/loaders/browserify.feature
@@ -1,0 +1,34 @@
+@no-default-plugin
+Feature: browserify + typescript
+  Scenario:
+    Given a file named "cypress/integration/a.feature" with:
+      """
+      Feature: a feature name
+        Scenario: a scenario name
+          Given a step
+      """
+    And a file named "cypress/plugins/index.ts" with:
+      """
+      import browserify from "@cypress/browserify-preprocessor";
+      import { preprocessor } from "@badeball/cypress-cucumber-preprocessor/browserify";
+
+      export default (
+        on: Cypress.PluginEvents,
+        config: Cypress.PluginConfigOptions
+      ): void => {
+        on(
+          "file:preprocessor",
+          preprocessor(config, {
+            ...browserify.defaultOptions,
+            typescript: require.resolve("typescript")
+          })
+        );
+      };
+      """
+    And a file named "cypress/support/step_definitions/steps.ts" with:
+      """
+      import { Given } from "@badeball/cypress-cucumber-preprocessor/methods";
+      Given("a step", function(this: Mocha.Context) {});
+      """
+    When I run cypress
+    Then it passes

features/loaders/esbuild.feature
@@ -1,0 +1,33 @@
+@no-default-plugin
+Feature: esbuild + typescript
+  Scenario:
+    Given a file named "cypress/integration/a.feature" with:
+      """
+      Feature: a feature name
+        Scenario: a scenario name
+          Given a step
+      """
+    And a file named "cypress/plugins/index.ts" with:
+      """
+      import createBundler from "@bahmutov/cypress-esbuild-preprocessor";
+      import { createEsbuildPlugin } from "@badeball/cypress-cucumber-preprocessor/esbuild";
+
+      export default (
+        on: Cypress.PluginEvents,
+        config: Cypress.PluginConfigOptions
+      ): void => {
+        on(
+          "file:preprocessor",
+          createBundler({
+            plugins: [createEsbuildPlugin(config)]
+          })
+        );
+      };
+      """
+    And a file named "cypress/support/step_definitions/steps.ts" with:
+      """
+      import { Given } from "@badeball/cypress-cucumber-preprocessor/methods";
+      Given("a step", function(this: Mocha.Context) {});
+      """
+    When I run cypress
+    Then it passes

features/loaders/webpack.feature
@@ -1,0 +1,58 @@
+@no-default-plugin
+Feature: webpack + typescript
+  Scenario:
+    Given a file named "cypress/integration/a.feature" with:
+      """
+      Feature: a feature name
+        Scenario: a scenario name
+          Given a step
+      """
+    And a file named "cypress/plugins/index.ts" with:
+      """
+      import webpack from "@cypress/webpack-preprocessor";
+
+      export default (
+        on: Cypress.PluginEvents,
+        config: Cypress.PluginConfigOptions
+      ): void => {
+        on(
+          "file:preprocessor",
+          webpack({
+            webpackOptions: {
+              resolve: {
+                extensions: [".ts", ".js"]
+              },
+              module: {
+                rules: [
+                  {
+                    test: /\.ts$/,
+                    exclude: [/node_modules/],
+                    use: [
+                      {
+                        loader: "ts-loader"
+                      }
+                    ]
+                  },
+                  {
+                    test: /\.feature$/,
+                    use: [
+                      {
+                        loader: "@badeball/cypress-cucumber-preprocessor/webpack",
+                        options: config
+                      }
+                    ]
+                  }
+                ]
+              }
+            }
+          })
+        );
+      };
+      """
+    And a file named "cypress/support/step_definitions/steps.ts" with:
+      """
+      import { Given } from "@badeball/cypress-cucumber-preprocessor/methods";
+      Given("a step", function(this: Mocha.Context) {});
+      """
+    When I run cypress
+    Then it passes

features/support/hooks.js
@@ -30,23 +30,6 @@ Before(async function ({ gherkinDocument, pickle }) {
     )
   );
 
-  await writeFile(
-    path.join(this.tmpDir, "cypress", "plugins", "index.js"),
-    `
-      const { createEsbuildPlugin } = require("${projectPath}/esbuild");
-      const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
-
-      module.exports = (on, config) => {
-        on(
-          "file:preprocessor",
-          createBundler({
-            plugins: [createEsbuildPlugin(config)]
-          })
-        );
-      }
-    `
-  );
-
   await fs.mkdir(path.join(this.tmpDir, "node_modules", "@badeball"), {
     recursive: true,
   });
@@ -59,6 +42,25 @@ Before(async function ({ gherkinDocument, pickle }) {
       "@badeball",
       "cypress-cucumber-preprocessor"
     )
+  );
+});
+
+Before({ tags: "not @no-default-plugin" }, async function () {
+  await writeFile(
+    path.join(this.tmpDir, "cypress", "plugins", "index.js"),
+    `
+      const { createEsbuildPlugin } = require("@badeball/cypress-cucumber-preprocessor/esbuild");
+      const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+
+      module.exports = (on, config) => {
+        on(
+          "file:preprocessor",
+          createBundler({
+            plugins: [createEsbuildPlugin(config)]
+          })
+        );
+      }
+    `
   );
 });
 

lib/index.ts
@@ -61,18 +61,22 @@ type ICypressPreprocessorFile = EventEmitter & {
   shouldWatch: boolean;
 };
 
-function preprendTransformerToOptions(options: any) {
+function preprendTransformerToOptions(
+  configuration: ICypressConfiguration,
+  options: any
+) {
   let wrappedTransform;
 
   if (
     !options.browserifyOptions ||
     !Array.isArray(options.browserifyOptions.transform)
   ) {
-    wrappedTransform = [transform];
-  } else if (!options.browserifyOptions.transform.includes(transform)) {
-    wrappedTransform = [transform, ...options.browserifyOptions.transform];
+    wrappedTransform = [transform.bind(null, configuration)];
   } else {
-    wrappedTransform = options.browserifyOptions.transform;
+    wrappedTransform = [
+      transform.bind(null, configuration),
+      ...options.browserifyOptions.transform,
+    ];
   }
 
   return {
@@ -84,8 +88,14 @@ function preprendTransformerToOptions(options: any) {
   };
 }
 
-export function preprocessor(options = browserify.defaultOptions) {
-  options = preprendTransformerToOptions(options);
+export function preprocessor(
+  configuration: ICypressConfiguration,
+  options = browserify.defaultOptions,
+  { prependTransform = true }: { prependTransform?: boolean } = {}
+) {
+  if (prependTransform) {
+    options = preprendTransformerToOptions(configuration, options);
+  }
 
   return function (file: ICypressPreprocessorFile) {
     return browserify(options)(file);

webpack.ts
@@ -1,0 +1,26 @@
+import { compile } from "./lib/template";
+
+/**
+ * Can be removed once Webpack v5 is supported by Cypress' preprocessor, because Webpack v5 ships
+ * with these types, ref. https://github.com/webpack/webpack/pull/13164.
+ */
+interface ILoaderContext {
+  async: () => (err: Error | null, result?: string) => void;
+  resourcePath: string;
+  query: any;
+}
+
+interface ILoaderDefinition {
+  (this: ILoaderContext, data: string): void;
+}
+
+const loader: ILoaderDefinition = function (data) {
+  const callback = this.async();
+
+  compile(this.query, data, this.resourcePath).then(
+    (result) => callback(null, result),
+    (error) => callback(error)
+  );
+};
+
+export default loader;