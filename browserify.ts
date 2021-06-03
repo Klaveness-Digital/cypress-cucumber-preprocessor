25b3d205dc6652aac69e5c460d3cc51bb9da147c
Jonas Amundsen <jonasba@gmail.com>
2021-06-03 14:01:17 +0200

    Avoid having to install peerDeps to access main
    The browserify peerDep is strictly only required when using browserify
    as a bundler. When you're only interested in the config-resolver
    helpers, then having to install browserify-preprocessor is undesirable.

--- browserify.ts
+++ browserify.ts
@@ -1,1 +1,97 @@
-export * from "./lib";
+import { PassThrough, Transform, TransformCallback } from "stream";
+
+import { EventEmitter } from "events";
+
+import browserify from "@cypress/browserify-preprocessor";
+
+import debug from "./lib/debug";
+
+import { compile } from "./lib/template";
+
+import { ICypressConfiguration } from "./lib/cypress-configuration";
+
+export function transform(
+  configuration: ICypressConfiguration,
+  filepath: string
+) {
+  if (!filepath.match(".feature$")) {
+    return new PassThrough();
+  }
+
+  debug(`compiling ${filepath}`);
+
+  let buffer = Buffer.alloc(0);
+
+  return new Transform({
+    transform(chunk: any, encoding: string, done: TransformCallback) {
+      buffer = Buffer.concat([buffer, chunk]);
+      done();
+    },
+    async flush(done: TransformCallback) {
+      try {
+        done(
+          null,
+          await compile(configuration, buffer.toString("utf8"), filepath)
+        );
+
+        debug(`compiled ${filepath}`);
+      } catch (e) {
+        done(e);
+      }
+    },
+  });
+}
+
+// https://docs.cypress.io/api/plugins/preprocessors-api.html#File-object
+type ICypressPreprocessorFile = EventEmitter & {
+  filePath: string;
+  outputPath: string;
+  shouldWatch: boolean;
+};
+
+function preprendTransformerToOptions(
+  configuration: ICypressConfiguration,
+  options: any
+) {
+  let wrappedTransform;
+
+  if (
+    !options.browserifyOptions ||
+    !Array.isArray(options.browserifyOptions.transform)
+  ) {
+    wrappedTransform = [transform.bind(null, configuration)];
+  } else {
+    wrappedTransform = [
+      transform.bind(null, configuration),
+      ...options.browserifyOptions.transform,
+    ];
+  }
+
+  return {
+    ...options,
+    browserifyOptions: {
+      ...(options.browserifyOptions || {}),
+      transform: wrappedTransform,
+    },
+  };
+}
+
+export function preprocessor(
+  configuration: ICypressConfiguration,
+  options = browserify.defaultOptions,
+  { prependTransform = true }: { prependTransform?: boolean } = {}
+) {
+  if (prependTransform) {
+    options = preprendTransformerToOptions(configuration, options);
+  }
+
+  return function (file: ICypressPreprocessorFile) {
+    return browserify(options)(file);
+  };
+}
+
+export { ICypressConfiguration };
+
+export { compile };
+
+export default preprocessor;
--- lib/index.ts
+++ lib/index.ts
@@ -1,16 +1,4 @@
-import { PassThrough, Transform, TransformCallback } from "stream";
-
-import { EventEmitter } from "events";
-
 import { messages } from "@cucumber/messages";
-
-import browserify from "@cypress/browserify-preprocessor";
-
-import debug from "./debug";
-
-import { compile } from "./template";
-
-import { ICypressConfiguration } from "./cypress-configuration";
 
 declare global {
   interface Window {
@@ -21,92 +9,6 @@ declare global {
     };
   }
 }
-
-export function transform(
-  configuration: ICypressConfiguration,
-  filepath: string
-) {
-  if (!filepath.match(".feature$")) {
-    return new PassThrough();
-  }
-
-  debug(`compiling ${filepath}`);
-
-  let buffer = Buffer.alloc(0);
-
-  return new Transform({
-    transform(chunk: any, encoding: string, done: TransformCallback) {
-      buffer = Buffer.concat([buffer, chunk]);
-      done();
-    },
-    async flush(done: TransformCallback) {
-      try {
-        done(
-          null,
-          await compile(configuration, buffer.toString("utf8"), filepath)
-        );
-
-        debug(`compiled ${filepath}`);
-      } catch (e) {
-        done(e);
-      }
-    },
-  });
-}
-
-// https://docs.cypress.io/api/plugins/preprocessors-api.html#File-object
-type ICypressPreprocessorFile = EventEmitter & {
-  filePath: string;
-  outputPath: string;
-  shouldWatch: boolean;
-};
-
-function preprendTransformerToOptions(
-  configuration: ICypressConfiguration,
-  options: any
-) {
-  let wrappedTransform;
-
-  if (
-    !options.browserifyOptions ||
-    !Array.isArray(options.browserifyOptions.transform)
-  ) {
-    wrappedTransform = [transform.bind(null, configuration)];
-  } else {
-    wrappedTransform = [
-      transform.bind(null, configuration),
-      ...options.browserifyOptions.transform,
-    ];
-  }
-
-  return {
-    ...options,
-    browserifyOptions: {
-      ...(options.browserifyOptions || {}),
-      transform: wrappedTransform,
-    },
-  };
-}
-
-export function preprocessor(
-  configuration: ICypressConfiguration,
-  options = browserify.defaultOptions,
-  { prependTransform = true }: { prependTransform?: boolean } = {}
-) {
-  if (prependTransform) {
-    options = preprendTransformerToOptions(configuration, options);
-  }
-
-  return function (file: ICypressPreprocessorFile) {
-    return browserify(options)(file);
-  };
-}
-
-export { ICypressConfiguration };
-
-export { compile };
-
-export default preprocessor;
 
 export {
   resolveConfiguration as resolveCypressConfiguration,
