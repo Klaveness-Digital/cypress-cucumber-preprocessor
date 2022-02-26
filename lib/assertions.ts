f9a50ad90e35f19d680dc5e71b8715941a3155e9
Jonas Amundsen <jonasba@gmail.com>
2022-02-26 13:43:29 +0100

    Add repo URL to all assertion errors

--- lib/assertions.ts
+++ lib/assertions.ts
@@ -1,16 +1,20 @@
 import { isString } from "./type-guards";
 
-export function assert(value: any, message?: string): asserts value {
+import { homepage } from "../package.json";
+
+export function assert(value: any, message: string): asserts value {
   if (value) {
     return;
   }
 
-  throw new Error(message ?? `Expected a truthy value, but got ${value}`);
+  throw new Error(
+    `${message} (this might be a bug, please report at ${homepage})`
+  );
 }
 
 export function assertAndReturn<T>(
   value: T,
-  message?: string
+  message: string
 ): Exclude<T, false | null | undefined> {
   assert(value, message);
   return value as Exclude<T, false | null | undefined>;
