3cc7e1ba777ae475893a3fe1a1a4e8ee98e7986f
Jonas Amundsen <jonasba@gmail.com>
2022-03-31 16:57:38 +0200

    Remove ../methods postfix
    This is now possible.

--- examples/esbuild/cypress/integration/duckduckgo.ts
+++ examples/esbuild/cypress/integration/duckduckgo.ts
@@ -1,4 +1,4 @@
-import { When, Then } from "@badeball/cypress-cucumber-preprocessor/methods";
+import { When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
 When("I visit duckduckgo.com", () => {
   cy.visit("https://duckduckgo.com/");
